e710395101ae8de4b6c1dcc3c2538c5c4693503f
Rakesh Katti <rakesh.katti@gmail.com>
2023-12-03 15:53:00 +0530

    add analytics adn fyi

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import React from "react";
 import styled from "styled-components";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import Script from "next/script";
 
 const Container = styled.div`
 	display: flex;
@@ -45,59 +46,88 @@ const BlogLink = styled.a`
 	}
 `;
 
+const FYIText = styled.p`
+	position: fixed; /* Fix the text at the bottom */
+	bottom: 10px; /* Adjust the distance from the bottom */
+	left: 50%; /* Center horizontally */
+	transform: translateX(-50%); /* Center horizontally */
+	font-size: 14px;
+	color: #888; /* Set your desired text color */
+`;
+
 const ComingSoon = () => {
 	return (
-		<Container>
-			{/* Your Name */}
-			<Heading>Rakesh Katti</Heading>
-			<SecondHeader>Coming Soon</SecondHeader>
+		<>
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-X50BBT8YX6"
+			></Script>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
 
-			{/* Coming Soon Message */}
-			<Message>
+					gtag('config', 'G-X50BBT8YX6');
+				`}
+			</Script>
+			<Container>
+				{/* Your Name */}
+				<Heading>Rakesh Katti</Heading>
+				<SecondHeader>Frontend Engineer</SecondHeader>
+
+				{/* Coming Soon Message */}
+				<Message>
+					<p>
+						This website is under construction and will be available shortly.
+						Stay tuned!
+						<br />
+						Follow on these co-ordinates to know more!
+					</p>
+				</Message>
+
+				{/* Social Media Icons with Links */}
+				<SocialIcons>
+					<a
+						href="https://www.linkedin.com/in/rakeshkatti/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FaLinkedin size={30} style={{ margin: "0 10px" }} />
+					</a>
+					<a
+						href="https://www.instagram.com/rakeshkatti/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FaInstagram size={30} style={{ margin: "0 10px" }} />
+					</a>
+					<a
+						href="https://twitter.com/rakesh_katti"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FaXTwitter size={30} style={{ margin: "0 10px" }} />
+					</a>
+				</SocialIcons>
+
+				{/* Link to Blog */}
 				<p>
-					This website is under construction and will be available shortly. Stay
-					tuned!
-					<br />
-					Follow on these co-ordinates to know more!
+					<BlogLink
+						href="https://blog.rakeshkatti.in/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Go to my Blog on Hashnode
+					</BlogLink>
 				</p>
-			</Message>
 
-			{/* Social Media Icons with Links */}
-			<SocialIcons>
-				<a
-					href="https://www.instagram.com/rakeshkatti/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FaInstagram size={30} style={{ margin: "0 10px" }} />
-				</a>
-				<a
-					href="https://twitter.com/rakesh_katti"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FaXTwitter size={30} style={{ margin: "0 10px" }} />
-				</a>
-				<a
-					href="https://www.linkedin.com/in/rakeshkatti/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FaLinkedin size={30} style={{ margin: "0 10px" }} />
-				</a>
-			</SocialIcons>
-
-			{/* Link to Blog */}
-			<p>
-				<BlogLink
-					href="https://blog.rakeshkatti.in/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Go to my Blog on Hashnode
-				</BlogLink>
-			</p>
-		</Container>
+				<FYIText>
+					My previous site rakeshkatti.com got hijacked. Thought it`&apos;s a
+					good time for a redesign.
+				</FYIText>
+			</Container>
+		</>
 	);
 };
 
